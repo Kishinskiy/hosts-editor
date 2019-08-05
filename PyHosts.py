87a90c9ce855335e28c3e88480fa75d0e3f356ed
Oleg Kishinskiy <oleg@kishinskiy.ru>
2019-08-05 11:31:53 +0300

    init final

---
+++ PyHosts.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+from python_hosts.hosts import Hosts, HostsEntry
+import argparse, sys
+import platform
+
+def parse_input():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', nargs='+', required=True,
+                        help='IP address like 127.0.0.1')
+    parser.add_argument('-n', nargs='*', required=True,
+                        help='domain name and alias like www.example.com example.com')
+
+    # no input means show me the help
+    if len(sys.argv) == 0:
+        parser.print_help()
+        sys.exit()
+
+    return parser.parse_args()
+
+
+def write_hosts(ip_address, domain_name, alias_name):
+    if platform.system() == 'Windows':
+        hosts_path="C:\\Windows\\System32\\drivers\\etc\\hosts"
+    else:
+        hosts_path="/etc/hosts"
+
+    hosts = Hosts(path=hosts_path)
+    hosts.remove_all_matching(name=domain_name)
+    new_entry = HostsEntry(entry_type='ipv4', address=ip_address, names=[domain_name, alias_name])
+    hosts.add([new_entry])
+    hosts.write()
+
+
+if __name__ == '__main__':
+    filename = "hosts"
+    arg = parse_input()
+    # print(arg.p)
+    ip_address = arg.p[0]
+    domain_name = arg.n[0]
+    alias_name = arg.n[1]
+
+    try:
+      write_hosts(ip_address, domain_name, alias_name)
+    except Exception as e:
+        print("Error Write to  Hosts", e)
+    else:
+        print("Domain name added")
---
+++ Readme.md
@@ -0,0 +1,11 @@
+# Hosts Editor
+
+## Usage:
+```bash
+python PyHosts.py -p Ip_address -h domain_name alias
+```
+
+## Example:
+``` bash
+python PyHosts.py -p 127.0.0.1 -h www.example.com example.com
+```
